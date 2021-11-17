663a75a9bd1bf616da956144e56959a3322605fb
Emtiaz310 <emtiazhossainrzs@gmail.com>
2021-11-17 21:01:12 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,25 @@
+const points = [40,20,30,100,300,2,14,31,30,59,395,28,82, 22,49,305,2049,294,394,20,573,39,57,48,99,99,23,394,22,22,100];
+document.getElementById('demo').innerHTML = points;
+
+function myFunction1(){
+    points.sort ();
+    document.getElementById('demo').innerHTML = points;
+}
+function myFunction2(){
+    points.sort(function(a,b){ 
+        return a - b ;
+    });
+    document.getElementById('demo').innerHTML = points ;
+}
+function myFunction3(){
+    points.sort(function(a,b){
+        return b - a ;
+    });
+    document.getElementById('demo').innerHTML = points;
+}
+function myFunction4(){
+    points.sort(function(a, b){
+        return 0.5 - Math.random() ;
+    });
+    document.getElementById('demo').innerHTML = points;
+}
