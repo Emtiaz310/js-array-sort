f6381037b2cd0b3b69a8d65cb40a441934e1e3e9
Emtiaz310 <emtiazhossainrzs@gmail.com>
2021-11-18 19:41:07 +0600

    date added

--- test.js
+++ test.js
@@ -23,3 +23,17 @@ function myFunction4(){
     });
     document.getElementById('demo').innerHTML = points;
 }
+
+// JavaScript Array Iteration 
+const numbers = [40,20,30,100,300,2,14,31,30,59,395,28,82, 22,49,305,2049,294,394,20,573,39,57,48,99,99,23,394,22,22,100];
+
+const over40 = numbers.filter(myFunction);
+
+document.getElementById('demo2').innerHTML = over40 ;
+
+function myFunction( value){
+    return value < 40 ; 
+};
+
+const d = new Date();
+document.getElementById('timed').innerHTML = d.toDateString();
